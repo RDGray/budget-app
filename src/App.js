ae1a8bb171b89da0f1dc22f4cde28c8f747f3628
RDGray <70549101+RDGray@users.noreply.github.com>
2022-01-18 04:14:35 +0300

    Modals Logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
-import React from "react";
 import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import AddBudgetModal from "./components/AddBudgetModal";
+import AddExpenseModal from "./components/AddExpenseModal";
 import BudgetCard from "./components/BudgetCard";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
+import { useState } from "react";
+import { useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState();
+  const { budgets, getBudgetExpenses } = useBudgets();
+
+  function openAddExpenseModal(budgetId) {
+    setShowAddExpenseModal(true);
+    setAddExpenseModalBudgetId(budgetId);
+  }
+
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budget</h1>
-        <Button variant="outline-success opacity-75">Add Budget</Button>
-        <Button variant="outline-danger opacity-50">Add Expense</Button>
-      </Stack>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
-        }}
-      >
-        <BudgetCard name="Entertainment" amount={100} max={1000}></BudgetCard>
-      </div>
-    </Container>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budget</h1>
+          <Button
+            variant="outline-success opacity-75"
+            onClick={() => setShowAddBudgetModal(true)}
+          >
+            Add Budget
+          </Button>
+          <Button
+            variant="outline-danger opacity-50"
+            onClick={openAddExpenseModal}
+          >
+            Add Expense
+          </Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          {budgets.map((budget) => {
+            const amount = getBudgetExpenses(budget.id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            );
+            return (
+              <BudgetCard
+                key={budget.id}
+                name={budget.name}
+                amount={amount}
+                max={budget.max}
+                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+              />
+            );
+          })}
+          <UncategorizedBudgetCard />
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+
+      <AddExpenseModal
+        show={showAddExpenseModal}
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() => setShowAddExpenseModal(false)}
+      />
+    </>
   );
 }
 
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -3,7 +3,13 @@ import React from "react";
 import { Card, ProgressBar, Stack, Button } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
-export default function BudgetCard({ name, amount, max, gray }) {
+export default function BudgetCard({
+  name,
+  amount,
+  max,
+  gray,
+  onAddExpenseClick,
+}) {
   const classNames = [];
   if (amount > max) {
     classNames.push("bg-danger", "bg-opacity-10");
@@ -17,19 +23,28 @@ export default function BudgetCard({ name, amount, max, gray }) {
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
             {currencyFormatter.format(amount)}
-            <span class="text-muted fs-6 ms-1">
-              / {currencyFormatter.format(max)}
-            </span>
+
+            {max && (
+              <span class="text-muted fs-6 ms-1">
+                / {currencyFormatter.format(max)}
+              </span>
+            )}
           </div>
         </Card.Title>
-        <ProgressBar
-          variant={getProgressBarVariant(amount, max)}
-          min={0}
-          max={max}
-          now={amount}
-        />
+        {max && (
+          <ProgressBar
+            variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
+        )}
         <Stack direction="horizontal" gap="2" className="mt-4">
-          <Button variant="outline-primary opacity-50" className=" ms-auto">
+          <Button
+            variant="outline-primary opacity-50"
+            className=" ms-auto"
+            onClick={onAddExpenseClick}
+          >
             Add Expense
           </Button>
           <Button variant="outline-secondary opacity-90">View Expense</Button>
